2f137d8dbb0e5934aa5ce50e9b68ef12eccb7187
Ani <a_grau05@wwu.de>
2018-07-25 17:07:14 +0200

    Layout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+"use strict";
+
+var express = require('express'); //Express functionality
+var router = express.Router(); //attaching a router variable to Express's router method
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Startpage' });
+});
+
+/* GET legal notice. */
+router.get('/imprint', function(req, res) {
+  res.render('imprint', { title: 'Legal notice' });
+});
+
+/* GET map1. */
+router.get('/map1', function(req, res) {
+  res.render('map1', { title: 'map1' });
 });
 
 module.exports = router;
