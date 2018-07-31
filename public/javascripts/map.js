b3e55cfe68e5d70c88f2409bfcec706ae901af5f
Ani <a_grau05@wwu.de>
2018-07-31 23:29:05 +0200

    Map_b

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,5 +1,15 @@
 "use strict";
 
+// creating map by using mapbox
 L.mapbox.accessToken = 'pk.eyJ1IjoiYW5pa2FnIiwiYSI6ImNqaWszMHZkYTAxcnYzcXN6OWl3NW5vdHkifQ.LeZkk6ZXp8VN1_PuToqTVA';
-  var map = L.mapbox.map('map', 'mapbox.streets')
-      .setView([51.95, 7.61], 11);
+  var map = L.mapbox.map('map').setView([51.95, 7.61], 11);
+
+// adding different layers to the map
+L.control.layers({
+
+  'Mapbox Streets': L.mapbox.tileLayer('mapbox.streets-basic').addTo(map),
+  'Mapbox Satellite': L.mapbox.tileLayer('mapbox.satellite'),
+  'Mapbox Dark': L.mapbox.tileLayer('mapbox.dark'),
+  'Mapbox Light': L.mapbox.tileLayer('mapbox.light')
+
+}).addTo(map);
