1017723ee3bdca94decdb94bbf09b9fc10e0b65c
Ani <a_grau05@wwu.de>
2018-07-31 18:40:19 +0200

    Map

---
+++ public/javascripts/map.js
@@ -0,0 +1,5 @@
+"use strict";
+
+L.mapbox.accessToken = 'pk.eyJ1IjoiYW5pa2FnIiwiYSI6ImNqaWszMHZkYTAxcnYzcXN6OWl3NW5vdHkifQ.LeZkk6ZXp8VN1_PuToqTVA';
+  var map = L.mapbox.map('map', 'mapbox.streets')
+      .setView([51.95, 7.61], 11);
