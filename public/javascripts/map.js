8a0442d3e649b1fd1d5fba76681a1a8dd01f5b33
Ani <a_grau05@wwu.de>
2018-08-04 20:59:48 +0200

    Fachbereiche fertig

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -68,3 +68,18 @@ function loadURL(){
     }
   })
 }
+
+function loadSubjectAreas(){
+
+  console.log("ich mache was");
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "/get-data", true);
+  xhttp.send();
+
+}
--- routes/index.js
+++ routes/index.js
@@ -28,9 +28,13 @@ router.get('/subject', function(req, res) {
   res.render('subject', { title: 'Add the subject areas!' });
 });
 
+/* saved_subject_areas. */
+router.get('/saved_subject_areas', function(req, res) {
+  res.render('saved_subject_areas', { title: 'Saved subject areas!' });
+});
 
-/* insert the form-data from the subject areas into mongodb */
-/* see: https://www.youtube.com/watch?v=ZKwrOXl5TDI */
+/* get the form-data from the subject areas from mongodb */
+/* see: https://github.com/mschwarzmueller/nodejs-basics-tutorial */
 router.post('/insert', function(req, res, next) {
   console.log('bis hier tuts!');
   var item = { // form in subject.jade
@@ -52,20 +56,24 @@ router.post('/insert', function(req, res, next) {
 res.redirect('subject'); // restart page
 });
 
-/* get the form-data from the subject areas from mongodb */
-/* see: https://www.youtube.com/watch?v=ZKwrOXl5TDI */
-router.get('/get-data', function(req, res, next) {
-  mongo.connect(url, function(err, db) { // connect to db
-    assert.equal(null, err); // check error
-    var cursor =  db.db('uniplaner').collection('subject_areas').find(); //cursor: pointing to the data which getting back
-                                                        //find: get all entries in the collection and stored in cursor
-      }, function() { //callback
-      db.close(); //close db
-      res.render('subject', {items: cursor}); //must be here, because the function above is ansynchronous and must first finished
-                                                  // page load again with the founded items (class in subject.jade)
-  });
+/* GET geolist page. */
+// extracting db object which is passed to the http request
+// using that db connection to fill the docs variable with database documents
+// page render
+router.get('/get-data', function(req, res) {
+    var db = req.db;
+    var collection = db.get('subject_areas');// tells the app which collection should be used
+    collection.find({},{},function(e,docs){// do a find
+        console.log(docs);
+        res.render('saved_subject_areas', {// render of geolist, giving it the userlist variable to work with
+            "subject_areas" : docs// passing the database documents to that variable
+        });
+    });
+
 });
 
+/* get the form-data from the subject areas from mongodb */
+/* see: https://github.com/mschwarzmueller/nodejs-basics-tutorial */
 router.post('/update', function(req, res, next) {
   var item = {
     name: req.body.name,
@@ -85,6 +93,8 @@ router.post('/update', function(req, res, next) {
   });
 });
 
+/* get the form-data from the subject areas from mongodb */
+/* see: https://github.com/mschwarzmueller/nodejs-basics-tutorial */
 router.post('/delete', function(req, res, next) {
   var id = req.body.id;
 
