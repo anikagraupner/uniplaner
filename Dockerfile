340832b1cacde26e1f8b7c303d0331c6997f5e37
Ani <a_grau05@wwu.de>
2018-09-02 22:54:38 +0200

    Projekt beendet

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
 # see: https://buddy.works/guides/how-dockerize-node-application
 FROM node:8
+
 WORKDIR /app
+
 COPY package.json /app
+
 RUN npm install
+
 COPY . /app
+
 EXPOSE 3000
-CMD ["node", "app.js"]
+
+CMD ["npm", "start"]
--- app.js
+++ app.js
@@ -14,10 +14,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
- app.listen(3000, function () {
-console.log("Express server listening on port 3000");
-});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
